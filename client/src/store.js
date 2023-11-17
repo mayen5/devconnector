8ac26b23020f780a907038bbaea8a7903c458abe
Carmelo Mayen <carmelo5mayen@gmail.com>
2023-11-17 14:58:07 -0600

    Prepared for deployment

--- client/src/store.js
+++ client/src/store.js
@@ -3,12 +3,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import alertReducer from './reducers/alert';
 import authReducer from './reducers/auth';
 import profileReducer from './reducers/profile';
+import postReducer from './reducers/post';
  
 const store = configureStore({
   reducer: {
     alert: alertReducer,
     auth: authReducer,
-    profile: profileReducer
+    profile: profileReducer,
+    post: postReducer
   }
 });
 
